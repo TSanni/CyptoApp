7990fe9e44c49e24cce692cc420d1a2d97b3c09a
TSanni <tomas.sanni@gmail.com>
2023-03-19 18:41:29 -0500

    Initial Commit

---
+++ CyptoApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CyptoApp
+//
+//  Created by Tomas Sanni on 3/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CyptoApp/CyptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyptoAppApp.swift
+//  CyptoApp
+//
+//  Created by Tomas Sanni on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct CyptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
