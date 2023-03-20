24a3263df0f8e26a9aae9e8899bf0a94b48cbc0e
TSanni <tomas.sanni@gmail.com>
2023-03-20 00:28:01 -0500

    Initial setup

--- CyptoApp/CyptoAppApp.swift
+++ CyptoApp/CyptoAppApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct CyptoAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                HomView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
---
+++ CyptoApp/Extensions/Color.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  CyptoApp
+//
+//  Created by Tomas Sanni on 3/19/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+
+
+}
